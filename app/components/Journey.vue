ee7c99c2bd3077e09755b5cc12a4779007ffdde3
Tyler Dickenson <TJDickenson75@gmail.com>
2025-08-08 01:39:36 +0100

    Fixing diamond height so it stays fixed when zoom in / zooming out

--- app/components/Journey.vue
+++ app/components/Journey.vue
@@ -4,31 +4,31 @@
       <h2 class="text-3xl font-bold mb-8 text-slate-800">My Journey</h2>
       
       <div class="relative flex justify-center">
-        <!-- Timeline vert bar -->
-        <div class="absolute h-full w-2 bg-slate-800 rounded-full"></div>
-        
-        <div class="min-h-[60vh] w-full relative">
+        <div class="w-full h-[800px] relative">
+          <div class="absolute left-1/2 -translate-x-1/2 top-0 h-[800px] w-2 bg-slate-800 rounded-full"></div>
+          
+
           <div class="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
             <div class="w-4 h-4 bg-slate-800 rotate-45"></div>
           </div>
-          <!-- First item card made to be made into some sort of component probs -->
-          <div class="absolute top-[-2px] left-[calc(50%+4px)] w-[calc(50vw-3rem)] md:w-80 bg-white shadow-md rounded-tr-md rounded-br-md p-6 border-r-4 border-b-4 border-t-4 border-slate-400">
+          
+          <div class="absolute top-0 left-[calc(50%+4px)] w-[calc(50vw-3rem)] md:w-80 bg-white shadow-md rounded-tr-md rounded-br-md p-6 border-r-4 border-b-4 border-t-4 border-slate-400">
             <div class="h-24"></div>
           </div>
           
-          <div class="absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+          <div class="absolute top-[200px] left-1/2 -translate-x-1/2 z-10">
             <div class="w-4 h-4 bg-slate-800 rotate-45"></div>
           </div>
           
-          <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+          <div class="absolute top-[400px] left-1/2 -translate-x-1/2 z-10">
             <div class="w-4 h-4 bg-slate-800 rotate-45"></div>
           </div>
           
-          <div class="absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
+          <div class="absolute top-[600px] left-1/2 -translate-x-1/2 z-10">
             <div class="w-4 h-4 bg-slate-800 rotate-45"></div>
           </div>
           
-          <div class="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 z-10">
+          <div class="absolute top-[800px] left-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
             <div class="w-4 h-4 bg-slate-800 rotate-45"></div>
           </div>
         </div>
