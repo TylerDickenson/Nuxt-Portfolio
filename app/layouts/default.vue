b3f209af3fe8246e79741daeb3ca40f19cd53b9b
Tyler Dickenson <TJDickenson75@gmail.com>
2025-08-21 16:02:21 +0100

    Fixing the right hand side blank area caused by dependancy vueuse/core

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="min-h-screen bg-cream flex flex-col">
     <Navigation />
+    <slot name="hero" />
     <main class="flex-1 container mx-auto px-4 py-8">
       <slot />
     </main>
-
     <ContactNav />
   </div>
 </template>
