47fd7af7f90f9c131370878b8c05bc91f27532b1
Tyler Dickenson <TJDickenson75@gmail.com>
2025-08-28 23:05:20 +0100

    Adding optimisation for the server to run the application

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,24 @@ export default defineNuxtConfig({
     '~/assets/css/main.css'
   ],
   
+  nitro: {
+    compressPublicAssets: true,
+    minify: true,
+    // Increase timeout for large assets
+    routeRules: {
+      '/images/**': { 
+        headers: { 'cache-control': 'public, max-age=31536000' },
+        prerender: false
+      }
+    }
+  },
+
+  build: {
+    transpile: ['aos'],
+  },
+
+  // Optimize for large assets
+  experimental: {
+    payloadExtraction: false
+  }
 })
